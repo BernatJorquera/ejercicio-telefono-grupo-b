e864cbdc35136be7bc6c1903f9132575932a56f0
Alejandro Gonzalez <alexgonzanu@gmail.com>
2021-03-10 13:54:24 +0100

    Cargado el html y css para visualizar la web

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 function App() {
   return (
-    <div>Hola</div>
+    <div className="contenedor">
+      <span className="mensaje">Llamando...</span>
+      <main className="telefono">
+        <div className="botones">
+          <ol className="teclado">
+            <li><button>1</button></li>
+            <li><button>2</button></li>
+            <li><button>3</button></li>
+            <li><button>4</button></li>
+            <li><button>5</button></li>
+            <li><button>6</button></li>
+            <li><button>7</button></li>
+            <li><button>8</button></li>
+            <li><button>9</button></li>
+            <li><button>0</button></li>
+            <li><button className="big">borrar</button></li>
+          </ol>
+        </div>
+        <div className="acciones">
+          <span className="numero">667359961</span>
+          <a href="llamar" className="llamar">Llamar</a>
+          <a href="colgar" className="colgar activo">Colgar</a>
+        </div>
+      </main>
+    </div>
   );
 }
 
